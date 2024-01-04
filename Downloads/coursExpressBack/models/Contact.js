5a6d30f8d7bf461d177a6dbea9bea15313d40df6
jihad-d <152963558+jihad-d@users.noreply.github.com>
2024-01-04 09:39:37 +0100

    Delete Downloads directory

--- Downloads/coursExpressBack/models/Contact.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const contactSchema = mongoose.Schema({
-    nom : { type: "string"}, //soit comme ça ou sans "" mais avec la premiere lettre en majucsule
-    prenom : { type: "string"},
-    email : { type : "string"},
-    message : { type : "string"},
-})
-
-module.exports = mongoose.model("Contact", contactSchema);
